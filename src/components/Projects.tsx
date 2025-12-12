49d06c6e9310e578de8a3a44a720a498d90e067b
Gustavo Simplício <AGENESEEZ@GMAIL.COM>
2025-12-12 05:32:11 -0300

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -111,7 +111,7 @@ const Projects: React.FC = () => {
                 <span>9 FAIXAS</span>
               </div>
               <p className="text-gray-400 text-sm mt-4 max-w-md">
-               O primeiro álbum de lançamentos da Geneseez tem como 
+               O primeiro álbum da Geneseez tem como 
                intuito explorar a musicalidade e talento dos artistas 
                DIIVINU e LOPZ através de composições autorais, o objetivo desse 
                projeto é obter resultados inovadores com todo o esforço e conhecimento 
